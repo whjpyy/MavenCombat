db65e55c715457a870096b7a72fb7801e4b03ae3
Rebort Chen <409121961@qq.com>
2015-09-06 23:34:50 +0800

    ch03 Maven使用入门

---
+++ hello-world/src/main/java/com/chen/mvnbook/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.chen.mvnbook.helloworld;
+
+public class HelloWorld{
+
+    public String sayHello(){
+        return "Hello Maven";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().sayHello());
+    }
+}
